335f9b9dab29f61caf52cf2546dbf23a9e8fed9f
allisondiego <allisondiego@gmail.com>
2021-11-08 21:03:30 -0300

    feat: adicionado logo

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import { Home } from '@/presentation/pages/home/home';
-import {AlertTemplate} from '@components/alert-template/alert-template'
 
 const Router: React.FC = () => {
     return (        
--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -4,9 +4,10 @@ import './home-styles.scss'
 export const Home: React.FC = () => {
   return (    
     <div className = "wrapper">
-      <div className = "info">
-        <h1>Bem Vindo</h1>
-        <p>Criado utilizando o  Modelo DevExtreme com Webpack</p>
+      <div className = "info">      
+        <img src="/img/logo-192x192.png"/>
+        <h1>DevExtreme com Webpack</h1>
+        <p>Criado utilizando o Modelo DevExtreme com Webpack</p>
       </div>      
     </div>
   );
